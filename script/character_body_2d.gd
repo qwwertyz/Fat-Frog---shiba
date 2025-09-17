c1b4d91869ee410c72722bfa7aa98287dc8b22f7
Zoe Yao <26yaoz86@gmail.com>
2025-09-17 22:16:15 +0200

    D2 randomness week. Small changes like adding text and changing player movement

--- script/character_body_2d.gd
+++ script/character_body_2d.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -450.0
+const SPEED = 4000.0
+const JUMP_VELOCITY = -500.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 
 func _physics_process(delta: float) -> void:
@@ -11,14 +13,38 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction<0:
+		animated_sprite_2d.flip_h = true
+	
+	#play animations
+	if is_on_floor():
+		if direction==0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
+		
+		
+	if direction and is_on_floor():
+		Engine.time_scale = 0.3
 		velocity.x = direction * SPEED
+		velocity.y = JUMP_VELOCITY
+		
+		while not is_on_floor():
+			velocity.x = direction * SPEED
+			
+		Engine.time_scale = 1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED) #speed is glide.
 
